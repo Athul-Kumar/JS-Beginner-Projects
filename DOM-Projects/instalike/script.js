def0c4ff7e176793e739c761c87422c1b0b54755
Athul-Kumar <athulkumar7991@gmail.com>
2023-11-23 12:20:58 +0530

    project completed

---
+++ DOM-Projects/instalike/script.js
@@ -0,0 +1,16 @@
+
+let container = document.querySelector('#container');
+let loveBtn = document.querySelector('.ri-heart-3-fill');
+
+container.addEventListener('dblclick',function(){
+
+    loveBtn.style.transform = 'translate(-50%,-50%) scale(1)';
+    loveBtn.style.opacity= 0.8;
+
+    setTimeout(function(){
+        loveBtn.style.opacity =0;
+    },1000)
+    setTimeout(function(){
+        loveBtn.style.transform = 'translate(-50%,-50%) scale(0)';
+    }, 3000)
+})
